6d6d788c40b949a83a94a89f78941daeffa3ba0a
HuuThongg <lehuuthong0506@gmail.com>
2022-11-30 23:41:13 -0800

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -140,19 +140,12 @@ const Navbar = () => {
         </nav>
       </div>
 
-      {/* <MenuOverlay> */}
-      {/* {toggle && ( */}
-      {/* <MenuIfor transform={transform} setTransform={setTransform} /> */}
       <div
         className={`overflow-hidden bg-white fixed inset-0 w-full h-full -translate-x-${transform} transition-all duration-500 ease-in-out  z-[100]`}
       >
         <div
-          // -translate-x-${transform}
           className={` overflow-hidden bg-white fixed inset-0 w-full h-full    z-[100]   opacity-100`}
         >
-          {/* <div
-          className={` overflow-hidden bg-white absolute inset-0 w-full h-full   z-[100] `}
-        > */}
           {/* header */}
           <div className=" bg-white flex justify-center items-center  min-h-[60px]  border-b-2 border-solid border-slate-400 w-full">
             <img src={logo} alt="" />
@@ -178,8 +171,6 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      {/* )} */}
-      {/* </MenuOverlay> */}
     </>
   );
 };
